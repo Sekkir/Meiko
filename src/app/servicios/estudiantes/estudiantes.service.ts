c4e8fe5c3502f3abb4d1c7154064f091ae612e10
Sekkir <carloshernandez016@gmail.com>
2024-11-25 23:21:37 -0600

    Update estudiantes.service.ts

--- src/app/servicios/estudiantes/estudiantes.service.ts
+++ src/app/servicios/estudiantes/estudiantes.service.ts
@@ -7,14 +7,14 @@ import { tap } from 'rxjs';
   providedIn: 'root'
 })
 export class EstudiantesService {
-  private apiUrl = 'http://localhost:3000';
+  private apiUrl = 'https://bemeiko-production.up.railway.app/api';
 
   constructor(private http: HttpClient) {}
 
   getEstudiantesByCurso(id_curso: number): Observable<any> {
     console.log("Solicitando estudiantes y puntajes para id_curso:", id_curso); // Verifica el id_curso enviado
   
-    return this.http.get<any>(`${this.apiUrl}/estudiantes/curso/${id_curso}/estudiantes`).pipe(
+    return this.http.get<any>(`${this.apiUrl}/curso/${id_curso}/estudiantes`).pipe(
       tap(response => {
         console.log("Respuesta de la API:", response); // Verifica la respuesta de la API
       })
@@ -23,12 +23,12 @@ export class EstudiantesService {
 
 
   getEstudianteDetalle(idEstudiante: number) {
-    return this.http.get(`${this.apiUrl}/estudiantes/seccion/${idEstudiante}`);
+    return this.http.get(`${this.apiUrl}/seccion/${idEstudiante}`);
   }
 
     // Método para obtener las tareas pendientes de un estudiante
     getTareasPendientes(id_estudiante: number): Observable<any> {
-      const url = `${this.apiUrl}/estudiantes/tareas-pendientes/${id_estudiante}`;
+      const url = `${this.apiUrl}/tareas-pendientes/${id_estudiante}`;
       return this.http.get<any>(url);
     }
 
