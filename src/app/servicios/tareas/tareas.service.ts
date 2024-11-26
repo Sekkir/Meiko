07ca10c75aed5db40b0dce6e02ea1ff559e742ce
Sekkir <carloshernandez016@gmail.com>
2024-11-25 23:47:27 -0600

    Update tareas.service.ts

--- src/app/servicios/tareas/tareas.service.ts
+++ src/app/servicios/tareas/tareas.service.ts
@@ -6,37 +6,37 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class TareasService {
-  private apiUrl = 'http://localhost:3000';  // URL de tu API
+  private apiUrl = 'https://bemeiko-production.up.railway.app/api';  // URL de tu API
 
   constructor(private http: HttpClient) { }
 
   // Método para obtener las tareas según curso y sección
   obtenerTareas(curso: number, seccion: number): Observable<any> {
     console.log(`Obteniendo tareas para curso: ${curso} y sección: ${seccion}`);
-    return this.http.get(`${this.apiUrl}/tareas/${curso}/${seccion}`);
+    return this.http.get(`${this.apiUrl}/${curso}/${seccion}`);
   }
 
   crearTarea(tarea: any) {
-    return this.http.post(`${this.apiUrl}/tareas/crear`, tarea);
+    return this.http.post(`${this.apiUrl}/crear`, tarea);
   }
 
   getDetalleTarea(id_tarea: number) {
     console.log('Solicitud al backend con id_tarea:', id_tarea);
-    return this.http.get<any>(`${this.apiUrl}/tareas/${id_tarea}`);
+    return this.http.get<any>(`${this.apiUrl}/${id_tarea}`);
   }
   
   getEstudiantesTarea(id_tarea: number) {
-    return this.http.get<any>(`${this.apiUrl}/tareas/estudiantes/${id_tarea}`);
+    return this.http.get<any>(`${this.apiUrl}/estudiantes/${id_tarea}`);
   }
 
     // Método para actualizar el estado del estudiante
   updateEstadoEstudiante(id_tarea_estudiante: number, estado: string) {
-    const url = `${this.apiUrl}/tareas/actualizar-estado/${id_tarea_estudiante}`;
+    const url = `${this.apiUrl}/actualizar-estado/${id_tarea_estudiante}`;
     return this.http.put(url, { estado }); // Enviamos el nuevo estado en el cuerpo de la solicitud
   }
 
   finalizarEntrega(idTarea: number) {
-    const url = `http://localhost:3000/tareas/finalizar/${idTarea}`;
+    const url = `${this.apiUrl}/finalizar/${idTarea}`;
     this.http.put(url, {}).subscribe(
       (response) => {
         console.log('Tarea finalizada correctamente:', response);
